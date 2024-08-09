d2dbfa57f04b7859614e9cbc34b9e456c8279a0b
Piyush Mahato <piyushmahato30@gmail.com>
2024-08-09 20:49:24 +0530

    get data from tvmaze api and display it. reusable function to fetch data from the api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,59 @@
 import { useState } from "react";
+import { searchForShows } from "../api/tvmade";
+
+
 const Home = () => {
-
     const [searchStr, setsearchStr] = useState("");
-    console.log(searchStr);
+    const [apiData, setApiData] = useState(null);
+    const [apiDataError, setApiDataError] = useState(null);
 
-
+    // setting search string 
     const onInputChange = (event) => {
         setsearchStr(event.target.value)
     }
 
     const onSearch = async (event) => {
+        setApiDataError(null);
+
+        // prevent from default action
         event.preventDefault();
-        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
-        const body = await response.json();
-        console.log(body)
-        // https://api.tvmaze.com/search/shows?q=boys
+
+        try {
+            const result = await searchForShows(searchStr);
+            setApiData(result);
+        } catch (error) {
+            setApiDataError(error)
+        }
+
     }
+    // conditional rending from the api data
+    const renderApiData = () => {
+        // checking the error
+        if (apiDataError) {
+            return <div>Error : {apiDataError.message}</div>
+        }
+        // checking data 
+        if (apiData) {
+            return apiData.map(data => (
+                <div key={data.show.id}>{data.show.name}</div>
+            ));
+        }
+
+        return null
+    }
+
+    // user view
     return (
         <div>
             <form onSubmit={onSearch}>
                 <input type="text" onChange={onInputChange} value={searchStr} />
                 <button type="submit">Search</button>
             </form>
+
+            <div>
+                {renderApiData()}
+            </div>
         </div>)
 }
+
 export default Home;
