58bb47364735c3e01ad124daaae3a34fa9f5abdf
Piyush Mahato <piyushmahato30@gmail.com>
2024-08-09 11:16:23 +0530

    added search bar, search for shows on button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
-
+import { useState } from "react";
 const Home = () => {
-    return (<div>
-        Home
-    </div>)
+
+    const [searchStr, setsearchStr] = useState("");
+    console.log(searchStr);
+
+
+    const onInputChange = (event) => {
+        setsearchStr(event.target.value)
+    }
+
+    const onSearch = async (event) => {
+        event.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
+        const body = await response.json();
+        console.log(body)
+        // https://api.tvmaze.com/search/shows?q=boys
+    }
+    return (
+        <div>
+            <form onSubmit={onSearch}>
+                <input type="text" onChange={onInputChange} value={searchStr} />
+                <button type="submit">Search</button>
+            </form>
+        </div>)
 }
 export default Home;
