52e4a8537471da18306952082c6186b57a75f67e
Piyush Mahato <piyushmahato30@gmail.com>
2024-08-12 11:39:14 +0530

    extract data fetching logic into a custom hook useShowById

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/tvmade";
 
-const Show = () => {
-    const { showId } = useParams();
-
+const useShowById = (showId) => {
     const [showData, setShowData] = useState(null);
-    const [showError, setShowError] = useState(null)
+    const [showError, setShowError] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
@@ -17,8 +15,14 @@ const Show = () => {
                 setShowError(error);
             }
         }
-        fetchData()
-    }, [showId])
+        fetchData();
+    }, [showId]);
+    return { showData, showError }
+}
+
+const Show = () => {
+    const { showId } = useParams();
+    const { showData, showError } = useShowById(showId)
 
     if (showError) {
         return <div>There is an error: {showError}</div>
